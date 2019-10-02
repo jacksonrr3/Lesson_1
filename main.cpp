4babcb160e7b640db58da840fab80f8cfa87e0e0
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2019-10-02 17:27:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 //#include <spdlog/spdlog.h>
 //#include "spdlog/fmt/ostr.h"
 //#include "spdlog/sinks/stdout_sinks.h"
-
+using namespace std
 
 int main()
   
  // auto logger = spdlog::stdout_logger_mt("console");
  // logger->info("build {}\n", version());
  // logger->info("Hello, world!\n");
-  std::cout << "build " << version() << "\n";
-  std::cout << "Hello, world!\n"; 
+  cout << "build " << version() << "\n";
+  cout << "Hello, world!\n"; 
 
 return 0;
